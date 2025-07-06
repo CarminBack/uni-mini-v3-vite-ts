b2b04a6769d07b58a408ed7ee2e9c7e65c830ca2
649985538@qq.com <649985538@qq.com>
2025-07-06 10:48:44 +0800

    数据请求错误处理

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,9 +1,11 @@
 import { useMemberStore } from '@/stores'
 
+// 定义请求的基础URL
 const baseURL = 'https://pcapi-xiaotuxian-front-devtest.itheima.net'
 
 // 添加拦截器
 const httpInterceptor = {
+  // 在请求前添加公共参数
   invoke(options: UniApp.RequestOptions) {
     // 在请求前添加公共参数
     if (!options.url.startsWith('http')) {
@@ -33,18 +35,41 @@ const httpInterceptor = {
 uni.addInterceptor('request', httpInterceptor)
 uni.addInterceptor('uploadFile', httpInterceptor)
 
-export const http = (options: UniApp.RequestOptions) => {
-  return new Promise((resolve, reject) => {
+// 封装请求函数
+interface Data<T> {
+  code: string
+  message: string
+  data: T
+}
+
+export const http = <T>(options: UniApp.RequestOptions) => {
+  return new Promise<Data<T>>((resolve, reject) => {
     uni.request({
       ...options,
       success: (res) => {
         if (res.statusCode === 200) {
-          resolve(res.data)
+          resolve(res.data as Data<T>)
+        } else if (res.statusCode === 401) {
+          // 获取用户信息存储
+          const memberStore = useMemberStore()
+          memberStore.clearProfile() // 清除用户信息
+          uni.navigateTo({
+            url: '/pages/login/login',
+          })
+          reject(new Error('Unauthorized access, please log in again.'))
         } else {
+          uni.showToast({
+            icon: 'none',
+            title: `${(res.data as Data<T>).message || '请求失败，请稍后再试'}`,
+          })
           reject(new Error(`HTTP error: ${res.statusCode}`))
         }
       },
       fail: (err) => {
+        uni.showToast({
+          icon: 'none',
+          title: `网络错误,换个网络试试`,
+        })
         reject(err)
       },
     })
